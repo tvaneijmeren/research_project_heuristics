0f913f1fd7d97fd94ec50ebde470c687e6ff5b08
Tiamo van Eijmeren <t.vaneijmeren@student.tudelft.nl>
2022-05-23 14:34:04 +0200

    Create results without cleanover heuristics

--- classes/heuristics.py
+++ classes/heuristics.py
@@ -12,67 +12,71 @@ def heuristic_slow(to_solve, csv_output):
             for w3 in range(-4, -3):
                 for w4 in range(-2, 1):
                     for w5 in range(-3, -2):
-                        to_solve = deepcopy(to_solve_original)
-                        z = -1
-                        y = -1
-                        for j in range(0, to_solve.maxJ):
-                            j_all_scheduled = True
-                            for job in to_solve.jobs:
-                                j_all_scheduled = j_all_scheduled and not job.operations[j].end_time == -1
-
-                            while(not j_all_scheduled):
-                                TC_ = to_solve.M
-
-                                for i_ in range(0, to_solve.n):
-                                    to_solve.isort = sorted(to_solve.jobs, key=(lambda x : x.s))  
-                                    job_i = to_solve.isort[i_]
-                                    i = job_i.index
-                                    if j < job_i.n_ops and job_i.operations[j].end_time == -1:
-                                        op_j = job_i.operations[j]
-                                        for k_ in range(0, to_solve.m):
-                                            # ksort -> lowest changeover times avg?
-                                            machine_k = to_solve.machines[k_]
-                                            k = machine_k.index
-                                            if machine_k.index in map((lambda x : x.index), op_j.machines):
-                                                #TODO implement heuristics
-                                                end_time_prev_op = 0
-                                                if j > 0:
-                                                    end_time_prev_op = job_i.operations[j-1].end_time
-                                                current_max = current_max_func(machine_k)
-                                                time_taken =  time_taken_func(op_j.machines, k)
-                                                h1 = max(current_max, end_time_prev_op) + time_taken
-                                                h2 = max(0, end_time_prev_op - current_max)
-                                                h3 = max(0, current_max - end_time_prev_op)
-                                                h4 = time_taken
-                                                h5 = job_i.s
-                                                TC = w1 * h1 + w2 * h2 + w3 * h3 + w4 * h4 + w5 * h5
-                                                if(TC < TC_):
-                                                    TC_ = TC
-                                                    z = i
-                                                    y = k
-
-                                if(TC_ < to_solve.M):
-                                    selected_machine = to_solve.machines[y]
-                                    selected_job = to_solve.jobs[z]
-
-                                    current_max = current_max_func(selected_machine)
-                                    start_time = current_max
-                                    prev_product = prev_product_func(selected_machine, start_time)
-                                    next_product = selected_job.product
-                                    if not start_time == 0:
-                                        changeover = to_solve.change_overs.get((y, prev_product, next_product))
-                                        start_time = start_time + changeover
-                                    start_time = max(start_time, selected_job.operations[j-1].end_time)
-                                    time_taken = time_taken_func(selected_job.operations[j].machines, y)
-                                    end_time = start_time + time_taken
-                                    to_solve.jobs[z].operations[j].end_time = end_time
-                                
-                                    to_solve.machines[y].operations.append({"Machine": y, "Job": z, "Product": next_product, "Operation": j, "Start": start_time, "Duration": time_taken, "Completion": end_time})
-
-                                j_all_scheduled = True
-                                for job in to_solve.jobs:
-                                    if j < job.n_ops:
+                        for w6 in [0, 1]:
+                            for w7 in [0, 1]:
+                                to_solve = deepcopy(to_solve_original)
+                                z = -1
+                                y = -1
+                                for j in range(0, to_solve.maxJ):
+                                    j_all_scheduled = True
+                                    for job in to_solve.jobs:
                                         j_all_scheduled = j_all_scheduled and not job.operations[j].end_time == -1
+
+                                    while(not j_all_scheduled):
+                                        TC_ = to_solve.M
+
+                                        for i_ in range(0, to_solve.n):
+                                            to_solve.isort = sorted(to_solve.jobs, key=(lambda x : x.s))  
+                                            ii = (1 - w6) * i_ + w6 * (to_solve.n - i_) 
+                                            job_i = to_solve.isort[ii]
+                                            i = job_i.index
+                                            if j < job_i.n_ops and job_i.operations[j].end_time == -1:
+                                                op_j = job_i.operations[j]
+                                                for k_ in range(0, to_solve.m):
+                                                    # ksort -> lowest changeover times avg?
+                                                    ki = (1 - w7) * k_ + w7 * (to_solve.m - k_) 
+                                                    machine_k = to_solve.machines[ki]
+                                                    k = machine_k.index
+                                                    if machine_k.index in map((lambda x : x.index), op_j.machines):
+                                                        #TODO implement heuristics
+                                                        end_time_prev_op = 0
+                                                        if j > 0:
+                                                            end_time_prev_op = job_i.operations[j-1].end_time
+                                                        current_max = current_max_func(machine_k)
+                                                        time_taken =  time_taken_func(op_j.machines, k)
+                                                        h1 = max(current_max, end_time_prev_op) + time_taken
+                                                        h2 = max(0, end_time_prev_op - current_max)
+                                                        h3 = max(0, current_max - end_time_prev_op)
+                                                        h4 = time_taken
+                                                        h5 = job_i.s
+                                                        TC = w1 * h1 + w2 * h2 + w3 * h3 + w4 * h4 + w5 * h5
+                                                        if(TC < TC_):
+                                                            TC_ = TC
+                                                            z = i
+                                                            y = k
+
+                                        if(TC_ < to_solve.M):
+                                            selected_machine = to_solve.machines[y]
+                                            selected_job = to_solve.jobs[z]
+
+                                            current_max = current_max_func(selected_machine)
+                                            start_time = current_max
+                                            prev_product = prev_product_func(selected_machine, start_time)
+                                            next_product = selected_job.product
+                                            if not start_time == 0:
+                                                changeover = to_solve.change_overs.get((y, prev_product, next_product))
+                                                start_time = start_time + changeover
+                                            start_time = max(start_time, selected_job.operations[j-1].end_time)
+                                            time_taken = time_taken_func(selected_job.operations[j].machines, y)
+                                            end_time = start_time + time_taken
+                                            to_solve.jobs[z].operations[j].end_time = end_time
+                                        
+                                            to_solve.machines[y].operations.append({"Machine": y, "Job": z, "Product": next_product, "Operation": j, "Start": start_time, "Duration": time_taken, "Completion": end_time})
+
+                                        j_all_scheduled = True
+                                        for job in to_solve.jobs:
+                                            if j < job.n_ops:
+                                                j_all_scheduled = j_all_scheduled and not job.operations[j].end_time == -1
                             
                         temp_results_unfolded = map((lambda x : x.operations), to_solve.machines)
                         temp_results = [x for y in temp_results_unfolded for x in y]
--- main_experiment.py
+++ main_experiment.py
@@ -3,6 +3,22 @@ import matplotlib.pyplot as plt
 from main_heuristics import heuristics_solve
 from main_milp import milp_solve
 
+a = [[(0, 23), (1, 36), (2, 47), (3, 60), (4, 70), (5, 82), (6, 92), (7, 105), (8, 115), (9, 128), (10, 138), (11, 151), (12, 161)], "Heuristic algorithm"]
+b = [[(0, 22.0), (1, 33.0), (2, 43.0), (3, 55.0), (4, 68.0), (5, 82.0), (6, 96.0), (7, 114.0), (8, 128.0), (9, 144.0), (10, 160.0), (11, 180.0), (12, 193.0)], "MILP Solver, 2700 second runtime limit"]
+
+def make_plot(solutions):
+    for solution in solutions:
+        x_val = list(map(lambda x : x[0], solution[0]))
+        y_val = list(map(lambda x : x[1], solution[0]))
+        plt.plot(x_val, y_val, marker='o', label= solution[1])
+    path = os.path.join("solutions/experiments")
+    plt.ylabel("Lowest makespan found")
+    plt.xticks(range(0, 13))
+    plt.xlabel("Instances")
+    plt.legend()
+    plt.savefig(path + "\\"  + "plot.png")
+
+
 
 # Runs an experiment. 
 # Name is the name of the folder where the results will be saved to.
@@ -27,4 +43,5 @@ def run_experiment(name, funcs_times_labels, nr_instances=13):
     plt.savefig(path + "\\" + name + ".png")
     
 
-run_experiment("heuristics", [(heuristics_solve, 30, "Heuristic solver")])
+make_plot([a, b])
+#run_experiment("heuristics", [(heuristics_solve, 30, "Heuristic solver")])
